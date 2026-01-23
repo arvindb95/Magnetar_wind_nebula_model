da0f5c202cdb3e3af5a2a4a4b6b8817621dd6603
Arvind Balasubramanian <arvind6895@gmail.com>
2026-01-23 13:35:52 +0530

    removed backup code

--- MWN_model.py
+++ MWN_model.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 import astropy.constants as const
 
+
 plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 
 m_e = (const.m_e.cgs).value
@@ -14,27 +15,42 @@ e = (const.e.esu).value
 
 
 def calc_N_gam_0(gam, xi, xi_min):
+    """
+    Calculates initial source term for the number density N_gamma
+    """
     theta = (196 / 3) * (xi / xi_min)
     beta = np.sqrt(1 - (1 / (gam**2)))
     return ((gam**2) * beta * np.exp(-gam / theta)) / (theta * kn(2, (1 / theta)))
 
 
 def calc_E_dot(t, t_0, alpha, B_16):
+    """
+    Calculates rate of energy injection
+    """
     E_b_star = 3e49 * (B_16) ** 2  # erg
     E_dot = (alpha - 1) * (E_b_star / t_0) * ((t / t_0) ** (-alpha))
     return E_dot
 
 
 def calc_N_e_dot(t, t_0, alpha, B_16, xi, sigma=0.1):
+    """
+    Rate of change of electron density
+    """
     E_dot = calc_E_dot(t, t_0, alpha, B_16)
     return E_dot / (1 + sigma) / xi
 
 
 def calc_Rn(t, v_n):
+    """
+    Radius of nebula
+    """
     return v_n * t  # returns cm
 
 
 def calc_EB_and_Bn(t, t_0, alpha, B_16, v_n, sigma):
+    """
+    Should'nt E_B be E_B_star initially?
+    """
     # time array should start exactly when the magnetar is born
     E_B = np.ones_like(t) * 1e50
     # ensuring everything is in cgs values so we input the erg/year unit instead of Gev/year
@@ -94,6 +110,28 @@ def calc_alpha_nu(gam, nu, N_gam, P_nu):
     return trapezoid(integrand, gam)
 
 
+def calc_L_nu_N_gam_t(t, gam, N_gamma_0, B_16, v_n, sigma):
+    N_gam_arr = np.zeros((len(gam), len(t)))
+    N_gam_arr[:, 0] = N_gamma_0
+    L_nu = np.zeros(len(t))
+
+    beta_sq = 1 - (1 / (gam) ** 2)
+
+    gamma_dot_adiab = np.zeros((len(gam), len(t - 1)))
+    gamma_dot_syn = np.zeros((len(gam), len(t - 1)))
+    gamma_dot_IC = np.zeros((len(gam), len(t - 1)))
+    gamma_dot_brem = np.zeros((len(gam), len(t - 1)))
+    E_B, B_n = calc_EB_and_Bn(t, t_0, alpha, B_16, v_n, sigma)
+
+    for i in range(len(t - 1)):
+        t_value = t[i + 1]
+        for j in range(len(gam)):
+            # gamma dot adiabatic
+            gamma_dot_adiab[j, i] = -gam[j] * beta_sq[j] * (1 / t[i])
+
+    return N_gam_arr
+
+
 # Test params
 
 gam = np.logspace(0, 5, 1000)
@@ -111,13 +149,16 @@ sigma = 0.1
 
 nu = 3e20
 
+print(calc_L_nu_N_gam_t(t, gam, calc_N_gam_0(gam, xi, xi_min), B_16, v_n, sigma)[:, 0])
+
+
 N_gamma_0 = calc_N_gam_0(
     gam,
     xi,
     xi_min,
 )
-
-E_B, B_N = calc_EB_and_Bn(t, t_0, alpha, B_16, v_n, sigma)
+"""
+set_xlabel, B_N = calc_EB_and_Bn(t, t_0, alpha, B_16, v_n, sigma)
 
 print(B_N[0])
 
@@ -131,6 +172,8 @@ P_nu = calc_P_nu(nu, nu_c, B_N[0])
 j_nu = calc_j_nu(gam, N_gamma_0, P_nu)
 
 print(j_nu)
+"""
+
 
 # Plotting
 """
